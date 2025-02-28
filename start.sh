747f335fbf77f5c13c5719daac37b4a82d9cfcbb
ngocsuo <hoangvanngoc37@gmail.com>
2025-03-01 05:32:35 +0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Sử dụng các biến môi trường để chạy Honeygain
+honeygain -tou-accept -email minhnam@hvnteam.com -pass Matkhau123 -device Re01 &
+
+# Sử dụng nc để lắng nghe trên port 8000
+while true; do
+    echo "HTTP/1.1 200 OK\r\n\r\nHealthy" | nc -l -p 8000
+done
